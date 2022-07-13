01bebd295909e02e29041505d6c6b422f303a482
Evgeniy <nanozeka@gmail.com>
2022-07-14 01:07:35 +0300

    add task_4

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите число №1");
+  int num_1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число №2");
+  int num_2 = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Введите число №3");
+  int num_3 = Convert.ToInt32(Console.ReadLine());
+  int max ;
+
+  if (num_1 > num_2)
+  max = num_1;
+  else max = num_2;
+
+  if (num_3 > max)
+  max = num_3;
+
+Console.WriteLine($"Число {max} максимальое");
